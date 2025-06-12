0134b410cc4037031995e448a1c6ed2489cb36e1
Zackk2504 <zackkingvn@gmail.com>
2025-06-12 18:09:48 +0700

    login with gg
    đăng nhập bằng gg

--- code/src/main/java/org/example/code/controller/LoginController.java
+++ code/src/main/java/org/example/code/controller/LoginController.java
@@ -1,10 +1,19 @@
 package org.example.code.controller;
 
+import org.example.code.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    private final AccountService accountService;
+
+    public LoginController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -13,5 +22,11 @@ public class LoginController {
     @GetMapping("/index")
     public String indexPage() {
         return "index"; // index.html
+    }
+
+    @GetMapping("/oauth2/success")
+    public ResponseEntity<?> handleOAuth2Success(Authentication authentication) {
+        accountService.LoginWithGG(authentication);
+        return ResponseEntity.ok("Đăng nhập thành công!");
     }
 }
--- code/src/main/java/org/example/code/service/AccountService.java
+++ code/src/main/java/org/example/code/service/AccountService.java
@@ -10,8 +10,12 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -70,5 +74,22 @@ public class AccountService {
             return true;
         }
         return false;
+    }
+
+    public void LoginWithGG(Authentication authentication) {
+        OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+        String email = oauthUser.getAttribute("email");
+
+        Optional<?> existingUser = Optional.ofNullable(accountRepository.findByEmail(email));
+        if (existingUser.isEmpty()) {
+            // Tạo mới user trong database
+            Taikhoan newUser = new Taikhoan();
+            newUser.setEmail(email);
+            newUser.setUsername(oauthUser.getAttribute("email")); // Sử dụng email làm username
+            newUser.setPass(passwordEncoder.encode("oauth2_default_password")); // Mật khẩu mặc định hoặc mã hóa
+            newUser.setHoTen(oauthUser.getAttribute("name"));
+            // random avatar hoặc dữ liệu mặc định
+            accountRepository.save(newUser);
+        }
     }
 }
